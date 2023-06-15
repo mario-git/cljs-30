1c242e73cb1fa75555660c8cac3ba27ec1cbbe1e
Mario Giampietri <mario.giampietri@gmail.com>
2023-06-15 19:40:34 +0100

    first drop of landing page (TBC)

--- src/cljs_30/main.cljs
+++ src/cljs_30/main.cljs
@@ -1,18 +1,28 @@
 (ns ^:figwheel-hooks cljs-30.main
-  (:require [cljs-30.day-01 :as d01]
-             ["react-dom/client" :refer [createRoot]]
-             [goog.dom :as gdom]
-             [reagent.core :as r]))
+  (:require [clojure.string :as s]
+            [cljs-30.day-01 :as d01]
+            ["react-dom/client" :refer [createRoot]]
+            [goog.dom :as gdom]
+            [reagent.core :as r]))
 
 (defonce app-state (atom {:text "Hello world!"}))
 
 (defonce root (createRoot (gdom/getElement "app")))
 
+(defn get-component-name [c] (-> c .-name (s/split "$") last))
+(def challenges [d01/drum-kit])
+
 (defn mount []
-  (.render root (r/as-element [:f> d01/drum-kit])))
+  (.render root (r/as-element
+                 [:div
+                  (map (fn [c] [:input {:type "button"
+                                        :on-click #(js/alert "to be bound yet! :)")
+                                        :value (get-component-name c)}])
+                       challenges)]
+                 #_[:f> d01/drum-kit])))
 
 (defn ^:after-load on-reload [] (mount)
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  ;; (swap! app-state update-in  [:__figwheel_counter] inc)
   )
 
 (defonce start-up (do (mount) true))
