93b1bc73daf81053f188808aebb49fb1f88765a3
Mario Giampietri <mario.giampietri@gmail.com>
2023-07-07 21:52:11 +0100

    js promise integ (somewhat) for day 6 + ditched comment in css

--- src/cljs_30/day_06.cljs
+++ src/cljs_30/day_06.cljs
@@ -1,6 +1,13 @@
 (ns cljs-30.day-06)
 
 (def ^:private endpoint "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json")
+(def ^:private cities-state (atom []))
+(def ^:private fetch-promise (-> (js/fetch endpoint) (.then #(.json %))))
+
+(defn cities []
+  (when (nil? @cities-state)
+    (.then fetch-promise #(reset! cities-state %)))
+  @cities-state)
 
 (defn type-ahead []
   [:form.search-form
